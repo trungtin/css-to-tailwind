a328968350ab63ccfa5208240a1e62a33d650575
Trần Trung Tín <trungtindev@gmail.com>
2022-08-03 18:20:27 +0700

    remove unused default tailwind.css

--- css-to-tailwind.js
+++ css-to-tailwind.js
@@ -1,18 +1,11 @@
-const postCss = require('postcss');
-const postCssTailwind = require('tailwindcss');
-const postCssAutoprefixer = require('autoprefixer');
 const transform = require('./lib/transform');
 const { setOptions } = require('./lib/options');
 
 async function cssToTailwind(inputCss, _options) {
+    // TODO: handle provided tailwind config
     const { TAILWIND_CONFIG, PREPROCESSOR_INPUT } = setOptions(_options)
 
-    const { css: tailwindCss } = await postCss([
-        postCssTailwind(TAILWIND_CONFIG || undefined),
-        postCssAutoprefixer,
-    ]).process(PREPROCESSOR_INPUT, { from: 'tailwind.css' });
-
-    return transform(inputCss, tailwindCss);
+    return transform(inputCss);
 }
 
 module.exports = cssToTailwind;
